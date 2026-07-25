87c675ba9b2691f56e8f1ebfee5b4c8d706a7dd9
Cato Kurtz <cato.kurtz@student.uni-tuebingen.de>
2026-07-25 20:22:22 +0200

    FIXME added button to show/hide results, but it does not quite work yet

--- tuebingen_search/web.py
+++ tuebingen_search/web.py
@@ -12,18 +12,33 @@ app = Flask(__name__)
 def index():
     results = []
     query = ""
+    #show = False
 
     if request.method == "POST":
-        query = request.form["query"]
+        if request.form["type"] == "query":
+            query = request.form["query"]
+            show = request.form["show_scores"]
 
-        if query.strip():
-            results = retrieve(query, "tuebingen_index.sqlite3", top_k=100)
+            if query.strip():
+                results = retrieve(query, "tuebingen_index.sqlite3", top_k=100)
 
-    return render_template(
-        "index.html",
-        query=query, 
-        results=results
-    )
+            return render_template(
+                "index.html",
+                query=query, 
+                results=results,
+                show=show
+            )
+        if request.form["type"] == "scores":
+            show = request.form["show_scores"] == "True"
+            query = request.form["query"]
+            results = request.form["results"]
+            return render_template(
+                "index.html",
+                query=query, 
+                results=results,
+                show = show
+            )
+
 
 def start_web_interface(host, port):
     print(f"starting web interface on {host}:{port}")
